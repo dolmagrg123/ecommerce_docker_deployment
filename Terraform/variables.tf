04865231351870489009536bb314e7ea90419cd3
Dolma <dolmatgurung1@gmail.com>
2024-11-17 15:52:14 -0500

    temp

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -1,12 +1,12 @@
-variable aws_access_key{
-    type=string
-    sensitive = true
-}
+# variable aws_access_key{
+#     type=string
+#     sensitive = true
+# }
 
-variable aws_secret_key{
-    type=string
-    sensitive = true
-}
+# variable aws_secret_key{
+#     type=string
+#     sensitive = true
+# }
 
 variable region{
     default = "us-east-1"
