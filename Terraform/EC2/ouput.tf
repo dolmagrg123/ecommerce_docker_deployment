86b8475b2390e4392b771e80ebf5f3d271dbbce3
Dolma <dolmatgurung1@gmail.com>
2024-11-19 17:33:47 -0500

    Updating lb

--- Terraform/EC2/ouput.tf
+++ Terraform/EC2/ouput.tf
@@ -6,13 +6,7 @@
    value = aws_security_group.backend_sg.id
  }
 
-#  output "eecommerce_bastion_az1_id"{
-#    value = aws_instance.ecommerce_bastion_az1.id
-#  }
-#  output "ecommerce_bastion_az2_id"{
-#    value = aws_instance.ecommerce_bastion_az2.id
-#  }
- output "eecommerce_app_az1_id"{
+ output "ecommerce_app_az1_id"{
    value = aws_instance.ecommerce_app_az1.id
  }
  output "ecommerce_app_az2_id"{
