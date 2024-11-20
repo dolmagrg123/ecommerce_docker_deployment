42ff9bea81ce9913246443167836a49f77477f1a
Dolma <dolmatgurung1@gmail.com>
2024-11-20 00:12:55 -0500

    rds endpoint

--- Terraform/RDS/output.tf
+++ Terraform/RDS/output.tf
@@ -2,7 +2,7 @@ output "rds_id" {
   value = aws_db_instance.main.id
 }
 output "rds_endpoint" {
-  value = aws_db_instance.main.endpoint
+  value = aws_db_instance.main.address
 }
 
 output "rds_db_name" {
